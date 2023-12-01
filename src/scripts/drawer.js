5b276392e2f3f5e6d106da8b6847177e1a42cab8
Diego Cavalcante <149342165+DiegoDev1312@users.noreply.github.com>
2023-12-01 17:00:29 -0300

    fix: ajustes no bug da responsividade

--- src/scripts/drawer.js
+++ src/scripts/drawer.js
@@ -7,7 +7,7 @@ const bgOpacity = document.querySelector('[data-bg-opacity]');
 function verifyDrawerSelected(selected) {
     const drawerOptions = menuDrawer.querySelectorAll('li');
     for (const option of drawerOptions) {
-        const isSelected = option.getAttribute('data-menu') === selected;
+        const isSelected = option.getAttribute('data-menu') === selected;        
         option.classList.toggle('bg-green-600', isSelected);
     }
 }
