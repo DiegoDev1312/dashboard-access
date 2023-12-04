6af3c7278156d59b8d5e61f64359e22b30ec3775
Diego Cavalcante <149342165+DiegoDev1312@users.noreply.github.com>
2023-12-04 15:39:30 -0300

    chore: correção da foto do perfil padrão

--- src/scripts/editPerfil.js
+++ src/scripts/editPerfil.js
@@ -1,5 +1,6 @@
 const perfilOptionArea = document.querySelector('[data-perfil-option]');
 const perfilButton = document.querySelector('[data-perfil-button]');
+const logoutButton = document.querySelector('[data-logout-button]');
 
 function closePerfilOption() {
     perfilOptionArea.classList.replace('block', 'hidden');
@@ -13,4 +14,15 @@ function handlePerfilPress() {
     }
 }
 
+function resetNavigationBrowser() {
+    window.history.replaceState(null, null, '../../index.html');
+    localStorage.removeItem('@user_persist');
+}
+
+function handleLogoutPress() {
+    window.location.href = '../../index.html';
+    resetNavigationBrowser();
+}
+
 perfilButton.addEventListener('click', handlePerfilPress);
+logoutButton.addEventListener('click', handleLogoutPress);
--- src/scripts/home.js
+++ src/scripts/home.js
@@ -58,7 +58,7 @@ function createPages(totalPage) {
     paginationArea.innerHTML = '';
     for (let i = 0; i < totalPage; i++) {
         const paginationButton = createElement('button'); 
-        const focusButtonValue = i + 1 === page ? 12 : 8;
+        const focusButtonValue = i + 1 === page ? 8 : 6;
         console.log(focusButtonValue);
         paginationButton.className = `h-${focusButtonValue} w-${focusButtonValue} bg-zinc-700 flex items-center justify-center text-white rounded`;
         paginationButton.textContent = i + 1;
