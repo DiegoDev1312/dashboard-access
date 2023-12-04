d55fe8bc831f9e218ff26811e19ffe9c12defca4
Diego Cavalcante <149342165+DiegoDev1312@users.noreply.github.com>
2023-12-04 11:13:17 -0300

    chore: adição da listagem de moradores

--- src/scripts/residents.js
+++ src/scripts/residents.js
@@ -1,1 +1,25 @@
+const residentsArea = document.querySelector('[data-residents-area]');
+
+function createResidents(resident) {
+    const modelComponents = document.querySelector('.resident-box').cloneNode(true);
+    const allInfoText = modelComponents.querySelectorAll('h2');
+    const userImage = modelComponents.querySelector('img');
+
+    allInfoText[0].textContent = resident.name;
+    allInfoText[1].textContent = resident.telephone;
+    allInfoText[2].textContent = resident.document;
+    allInfoText[3].textContent = resident.apartmentNumber;
+    userImage.src = resident.photo;
+
+    residentsArea.appendChild(modelComponents);
+}
+
+function renderResidents() {
+    for (const resident of residentsJson) {
+        createResidents(resident);
+    }
+
+}
+
+renderResidents();
 verifyDrawerSelected('residents');
